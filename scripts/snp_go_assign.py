05a734a049dc3b0142258f53e586a2a4f19d93ba
Reid Brennan <reid.brennan@gmail.com>
2017-12-14 17:21:08 -0500

    adding spu assignment to go script

--- scripts/snp_go_assign.py
+++ scripts/snp_go_assign.py
@@ -10,38 +10,70 @@ with open('/users/r/b/rbrennan/urchin_af/analysis/cmh.out.sorted.txt') as inf:
 
 sig_col = sig_col[1:]
 # assign GO terms
-filepath = '/users/r/b/rbrennan/reference/blast2go-whl.annot.txt.gz'
-
+filepath = '/users/r/b/rbrennan/reference/blast2go-whl.nospace.txt.gz'
+spu_path = '/users/r/b/rbrennan/reference/whl22.v1.0.tmap.gz'
 ####
 ## all loci
 ####
 
 #make empty array
-go_out = np.empty(shape=(0,2))
-i=0
+go_out = np.empty(shape=(0,3))
+spu_out = np.empty(shape=(0,2))
+
+i=0 # start counter
+
+# in col 3 of whl-annot, there are gene names. add these
 with open('/users/r/b/rbrennan/urchin_af/analysis/cmh.all.id') as master_file:
         for line in master_file:
             tmp_go = line.split("\n")[0]
+            tmp_spu = line.split("\n")[0]
+            # assign spu values
+            with gzip.open(spu_path) as spu_file:
+                for spu_line in spu_file:
+                    #make empty array to add results to
+                    if line.split("\n")[0] == spu_line.split("\t")[3].split("\n")[0]:
+                        if len(tmp_spu.split()) == 1:
+                            #print(gene_line)
+                            tmp_spu = tmp_spu + "\t" + spu_line.split("\t")[1].split("\n")[0]
+
+                        if len(tmp_spu.split()) > 1:
+                            tmp_spu = tmp_spu + ";" +  spu_line.split("\t")[1].split("\n")[0]
+
+            if len(tmp_spu.split()) == 1:
+                tmp_spu = tmp_spu + "\t" + "NA"
+            spu_out = np.vstack((spu_out, tmp_spu.split()))
+
+            # match GO terms
             with gzip.open(filepath) as file:
                 for gene_line in file:
-                    #print(gene_line)
                     #make empty array to add results to
                     if line.split("\n")[0] == gene_line.split("\t")[0]:
                         if len(tmp_go.split()) == 1:
-                            tmp_go = tmp_go + "\t" + gene_line.split("\t")[1].split("\n")[0]
+                            #print(gene_line)
+                            if len(gene_line.split("\t")) == 3:
+                                tmp_go = tmp_go + "\t" + gene_line.split("\t")[2].split("\n")[0]
+                                tmp_go = tmp_go + "\t" + gene_line.split("\t")[1].split("\n")[0]
+                                #print(gene_line)
+                            if len(gene_line.split("\t")) == 2:
+                                #tmp_go = tmp_go + "\t" + "NA"
+                                tmp_go = tmp_go + "\t" + gene_line.split("\t")[1].split("\n")[0]
                         if len(tmp_go.split()) > 1:
                             tmp_go = tmp_go + ";" +  gene_line.split("\t")[1].split("\n")[0]
             if len(tmp_go.split()) == 1:
-                tmp_go = tmp_go + "\t" + "unknown"
+                tmp_go = tmp_go + "\t" + "NA" + "\t" + "unknown"
+
             go_out = np.vstack((go_out, tmp_go.split()))
             i=i+1
-            if i % 500 == 0: print(i)
+            if i % 50 == 0: print(i)
+
+go_out_go = [i[2] for i in go_out]
+gene_name_out = [i[1] for i in go_out]
 
 np.savetxt('/users/r/b/rbrennan/urchin_af/analysis/cmh.all.GO', go_out,fmt='%5s', delimiter='\t')
 
-# then join together sig or not, snp name, etc
+#combine go and spu
+go_spu_out = np.column_stack((gene_name_out,spu_out, go_out_go))
 
-out_1 = np.column_stack((go_out, sig_col))
 
 # pull out snp ID
 i=0
@@ -64,8 +96,12 @@ with open('/users/r/b/rbrennan/urchin_af/analysis/cmh.out.sorted.txt') as master
         i=i+1
         if i % 10000 == 0: print(i)
 
-out_3 = np.column_stack((snp_out, out_1))
-head = "SNP" + "\t" + "CHR" + "\t" + "POS" + "\t" + "PVAL" + "\t" "WHL" + "\t" + "GO" + "\t" + "sig" + "\t" + "test_stat"
+# then join together sig or not, snp name, etc
+
+out_1 = np.column_stack((snp_out, sig_col))
+
+out_3 = np.column_stack((out_1, go_spu_out))
+head = "SNP" + "\t" + "CHR" + "\t" + "POS" + "\t" + "PVAL" + "\t" "test_stat" + "\t" + "sig"  + "\t" "gene_name" + "\t" "WHL" + "\t" "SPU"  + "\t" + "GO"
 head = head.split("\t")
 out_final = np.vstack((head, out_3))
 np.savetxt('/users/r/b/rbrennan/urchin_af/analysis/cmh.master.out', out_final,fmt='%5s', delimiter='\t')
