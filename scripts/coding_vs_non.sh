a6610043cfe7cd2450a2b49c5d2829c49347ca76
Reid Brennan <reid.brennan@gmail.com>
2018-01-17 13:48:48 -0500

    updated coding vs non assignment

--- scripts/coding_vs_non.sh
+++ scripts/coding_vs_non.sh
@@ -1,44 +1,40 @@
-cat ~/reference/urchin_probes.sorted.bed | awk 'BEGIN{FS=OFS="\t"} {gsub(" ", "", $1)}1' > ~/reference/urchin_probes.sorted.nospace.bed
+# this script counts the number of variants falling in coding vs non-coding regions. based on
+# the Strongylocentrotus_purpuratus.Spur_3.1.37.gff3.gz gff.
 
-echo "number of variants in exons"
-zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="exon" {print $0}'| wc -l
+zcat ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37.gff3.gz  | grep -v 'supercontig' | sort -k1,1 -k4,4n > ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3
 
-echo "number of exon probes represented"
-zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="exon" {print $0}' | cut -f 7 | sort | uniq | wc -l
+# count variants in exons:
+echo "number of variants coding"
 
-echo "number of variants in regulatory"
-zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="regulatory" {print $0}'| wc -l
+zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -wa -wb \
+      | grep -v 'three_prime_UTR' | cut -f 1-3 | sort | uniq | wc -l
 
-echo "number of regulatory probes represented"
-zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="regulatory" {print $0}'| cut -f 7 | sort | uniq | wc -l
+# save variants in exons:
+zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -wa -wb \
+      | cut -f 1-3 | sort | uniq > ~/urchin_af/analysis/variants_exon.bed
 
-zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first
+# count variants in non-coding:
+echo "number of variants non-coding"
+zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -v | wc -l
 
+# save variants in non-coding:
+zcat ~/urchin_af/variants/urchin_final.vcf.gz | grep -v '^#' | awk '{OFS="\t"; {print "chr"$1,$2-1,$2}}' |sort -k1,1 -k2,2n | sed 's/^chr//g'| ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -v > ~/urchin_af/analysis/variants_noncoding.bed
 
-
-######
+###
+# selected loci
 ##
-## look at prop of sig genes in coding/non
-##
-#######
+
+# count selected in exons
+echo "number of significant variants in coding"
+cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g' | ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -wa -wb \
+    | cut -f 1-3 | sort | uniq | wc -l
+
+cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g' | ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -wa -wb \
+    | cut -f 1-3 | sort | uniq > ~/urchin_af/analysis/variants_selected_exon.bed
 
 echo "number of significant variants in regulatory"
-cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g'|  ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="regulatory" {print $0}' | wc -l
+cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g' | ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -v \
+    | cut -f 1-3 | sort | uniq | wc -l
 
-echo "number of regulatory probes represented for sig variants"
-cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g'|  ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="regulatory" {print $0}' | cut -f 7 | sort | uniq | wc -l
-
-echo "number of significant variants in regulatory"
-cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g'|  ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="exon" {print $0}' | wc -l
-
-echo "number of regulatory probes represented for sig variants"
-cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g'|  ~/bin/bedtools2/bin/bedtools closest -a stdin -b ~/reference/urchin_probes.sorted.nospace.bed \
-    -d -t first | awk ' $9=="exon" {print $0}' | cut -f 7 | sort | uniq | wc -l
+cat ~/urchin_af/analysis/cmh.selected.bed| sort -k1,1 -k2,2n | sed 's/^chr//g' | ~/bin/bedtools2/bin/bedtools intersect -a stdin -b ~/reference/Strongylocentrotus_purpuratus.Spur_3.1.37_sorted.gff3 -v \
+    | cut -f 1-3 | sort | uniq > ~/urchin_af/analysis/variants_selected_noncoding.bed
