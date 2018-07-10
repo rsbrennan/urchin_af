ca271144e7917f59285466db980464b32d107172
Reid S Brennan <reid.brennan@gmail.com>
2018-07-10 10:25:17 -0400

    pres figs

--- scripts/01.1_variant_call.sh
+++ scripts/01.1_variant_call.sh
@@ -2,4 +2,6 @@
 
 # call variants w/ samtools. run in parallel, 10 jobs
 
+# the probe bed file is just being used to break the variant calling into scaffolds that contain capture probes. This should make things run more quickly. Will merge variants after.
+
 tail -n +2 /data/OASV2/RG_7601_Probe.extended.bed | cut -f 1 | sort | uniq| grep -v "#scaffold" | xargs -I {} -n 1 -P 10 sh -c "/data/programs/samtools-1.4.1/samtools mpileup -u --skip-indels -d 10000  -t DP,AD,INFO/AD -f /data/OASV2/Spur_3.1.LinearScaffold.fa -r {} /data/OASV2/bwamem/fullymapped/bwamem_final.merged.sorted.bam | /data/programs/bcftools/bin/bcftools call -mv - > ~/urchin_af/variants/temp/tmp.{}.vcf"
