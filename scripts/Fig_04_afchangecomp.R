65b4f328f63ca496ba23be5277b365f1e8299298
Reid S Brennan <reid.brennan@gmail.com>
2019-01-02 11:38:12 -0500

    change q to p val

--- scripts/Fig_04_afchangecomp.R
+++ scripts/Fig_04_afchangecomp.R
@@ -8,42 +8,43 @@ library(gridBase)
 library(grid)
 library(ggplot2)
 
-mydata <- read.table("~/urchin_af/analysis/cmh.out.txt", header=TRUE)
-cut_off <- 0.001
+mydata <- read.table("~/urchin_af/analysis/adaptive_allelefreq.txt", header=TRUE)
+cut_off <- 0.05/(9828)
 
-selected_7 <- mydata[which(mydata$pH7_selection_qval < cut_off & mydata$pH8_selection_qval >= cut_off),]
-selected_8 <- mydata[which(mydata$pH8_selection_qval < cut_off & mydata$pH7_selection_qval >= cut_off),]
-selected_both <- mydata[which(mydata$pH8_selection_qval < cut_off & mydata$pH7_selection_qval < cut_off),]
+selected_7 <- mydata[which(mydata$pH7_selection_pval < cut_off & mydata$pH8_selection_pval >= cut_off),]
+selected_8 <- mydata[which(mydata$pH8_selection_pval < cut_off & mydata$pH7_selection_pval >= cut_off),]
+selected_both <- mydata[which(mydata$pH8_selection_pval < cut_off & mydata$pH7_selection_pval < cut_off),]
+neutral <- mydata[which(mydata$pH8_selection_pval > cut_off & mydata$pH7_selection_pval > cut_off),]
 
-selected_7_D7_8 <- selected_7[,grep("D7_8_mean", colnames(selected_7))]
-selected_7_D7_7 <- selected_7[,grep("D7_7_mean", colnames(selected_7))]
-selected_7_D1_8 <- selected_7[,grep("D1_8_mean", colnames(selected_7))]
+# pull out allele freqs
+selected_7_D7_8 <- selected_7$D7_8_af
+selected_7_D7_7 <- selected_7$D7_7_af
+selected_7_D1_8 <- selected_7$D1_8_af
 
-selected_8_D7_8 <- selected_8[,grep("D7_8_mean", colnames(selected_8))]
-selected_8_D7_7 <- selected_8[,grep("D7_7_mean", colnames(selected_8))]
-selected_8_D1_8 <- selected_8[,grep("D1_8_mean", colnames(selected_8))]
+selected_8_D7_8 <- selected_8$D7_8_af
+selected_8_D7_7 <- selected_8$D7_7_af
+selected_8_D1_8 <- selected_8$D1_8_af
 
-selected_both_D7_8 <- selected_both[,grep("D7_8_mean", colnames(selected_both))]
-selected_both_D7_7 <- selected_both[,grep("D7_7_mean", colnames(selected_both))]
-selected_both_D1_8 <- selected_both[,grep("D1_8_mean", colnames(selected_both))]
+selected_both_D7_8 <- selected_both$D7_8_af
+selected_both_D7_7 <- selected_both$D7_7_af
+selected_both_D1_8 <- selected_both$D1_8_af
 
 selected_all <- unique(rbind(selected_7,selected_8,selected_both))
-selected_all_D7_8 <- selected_all[,grep("D7_8_mean", colnames(selected_all))]
-selected_all_D7_7 <- selected_all[,grep("D7_7_mean", colnames(selected_all))]
-selected_all_D1_8 <- selected_all[,grep("D1_8_mean", colnames(selected_all))]
+selected_all_D7_8 <- selected_all$D7_8_af
+selected_all_D7_7 <- selected_all$D7_7_af
+selected_all_D1_8 <- selected_all$D1_8_af
 
+d7_8 <- (selected_all_D7_8 - selected_all_D1_8)
+d7_7 <- (selected_all_D7_7 - selected_all_D1_8)
 
-d7_8 <- abs(selected_all_D1_8 - selected_all_D7_8)
-d7_7 <- abs(selected_all_D1_8 - selected_all_D7_7)
+d7_8_both <- (selected_both_D7_8 - selected_both_D1_8)
+d7_7_both <- (selected_both_D7_7 - selected_both_D1_8)
 
-d7_8_both <- abs(selected_both_D1_8 - selected_both_D7_8)
-d7_7_both <- abs(selected_both_D1_8 - selected_both_D7_7)
+d7_8_s7 <- (selected_7_D7_8 - selected_7_D1_8)
+d7_7_s7 <- (selected_7_D7_7 - selected_7_D1_8)
 
-d7_8_s7 <- abs(selected_7_D1_8  - selected_7_D7_8)
-d7_7_s7 <- abs(selected_7_D1_8  - selected_7_D7_7)
-
-d7_8_s8 <- abs(selected_8_D1_8  - selected_8_D7_8)
-d7_7_s8 <- abs(selected_8_D1_8  - selected_8_D7_7)
+d7_8_s8 <- (selected_8_D7_8 - selected_8_D1_8)
+d7_7_s8 <- (selected_8_D7_7 - selected_8_D1_8)
 
 
 cor.test(d7_7, d7_8,
@@ -61,9 +62,23 @@ cor.test(d7_7_both, d7_8_both,
          conf.level = 0.95)
 
 
+# corr genome wide:
+
+d7_8_all <- (neutral$D7_8_af - neutral$D1_8_af)
+d7_7_all <- (neutral$D7_7_af - neutral$D1_8_af)
+
+plot(d7_8_all, d7_7_all)
+
+cor.test(d7_8_all, d7_7_all,
+         method = "pearson",
+         conf.level = 0.95)
+
+
+library(scales)
+
 tiff("~/urchin_af/figures/Fig_04_afchangecomp.tiff", height=85, width=170, units="mm", res=300)
 par(mfrow = c(1, 2), mar=c(3, 3, 1.7, 1), mgp=c(3, 1, 0), las=0)
-plot(0,type='n', xlim=c(0,.39), ylim=c(0,.39),
+plot(0,type='n', xlim=c(-0.03,.39), ylim=c(-0.03,.39),
     main="",
     ylab="",
     xlab="",
@@ -78,7 +93,8 @@ points(x=d7_8_both, y=d7_7_both, col=alpha("darkorchid4", 0.7),
     bg = alpha("darkorchid4", 0.7), cex=0.8, pch=24)
 
 abline(0, 1, col="black", lty=2, lwd=2.2)
-
+abline(h=0, lty=2, col="grey")
+abline(v=0, lty=2, col="grey")
 axis(1, mgp=c(1.8, .2, 0), cex.axis=0.7,tcl=-0.2) # second is tick mark labels
 axis(2, mgp=c(1.8, .4, 0), cex.axis=0.7, tcl=-0.2)
 title(ylab=expression(paste(Delta," allele frequency pH 7.5")), line=1.5, cex.lab=0.9)
@@ -90,14 +106,14 @@ legend("topleft", c("pH 7.5 selected",
     horiz = FALSE, inset = c(0, 0),
     col = c("firebrick3", "royalblue3","darkorchid4"),
     pt.bg = c("firebrick3", "royalblue3","darkorchid4"),
-    pt.cex=1, cex=0.6, pch=c(21,22,24))
+    pt.cex=1, cex=0.6, pch=c(21,22,24),
+    bg="white")
 
 
 mtext(text=bquote(paste('(',italic('a'),')')),
               side=3, line=0,
              cex=1.5,
             at=par("usr")[1]-0.14*diff(par("usr")[1:2]), outer=FALSE)
-
 ######
 ##
 ## maf plot
@@ -105,12 +121,12 @@ mtext(text=bquote(paste('(',italic('a'),')')),
 ######
  #sampling from entire dist
 
-mydata <- read.table("~/urchin_af/analysis/adaptive_allelefreq.txt", stringsAsFactors=FALSE, header=TRUE)
-cut_off <- 0.001
+mydata <- read.table("~/urchin_af/analysis/adaptive_allelefreq.txt", header=TRUE)
+
 # need to pull out only selected alleles
-snp.sel_75 <- mydata$af_out[which(mydata$pH7_selection_qval < cut_off & mydata$pH8_selection_qval >= cut_off)]
-snp.sel_80 <- mydata$af_out[which(mydata$pH8_selection_qval < cut_off & mydata$pH7_selection_qval >= cut_off)]
-snp.sel_both <- mydata$af_out[which(mydata$pH8_selection_qval < cut_off & mydata$pH7_selection_qval < cut_off)]
+snp.sel_75 <- mydata$D1_8_af[which(mydata$pH7_selection_pval < cut_off & mydata$pH8_selection_pval >= cut_off)]
+snp.sel_80 <- mydata$D1_8_af[which(mydata$pH8_selection_pval < cut_off & mydata$pH7_selection_pval >= cut_off)]
+snp.sel_both <- mydata$D1_8_af[which(mydata$pH8_selection_pval < cut_off & mydata$pH7_selection_pval < cut_off)]
 
 nrep <- 1000
 nsamp <- length(snp.sel_75)
@@ -118,24 +134,34 @@ nsamp <- length(snp.sel_75)
 bs <- matrix(nrow=nsamp, ncol=nrep)
 avg_rep <- c()
 for(i in 1:nrep){
-    bs[,i] <- sample(mydata$af_out, size=nsamp, replace=FALSE)
+    bs[,i] <- sample(mydata$D1_8_af, size=nsamp, replace=FALSE)
     avg_rep <- c(avg_rep, bs[,i])
 }
 
 # calc ks tests
-p_75 <- c()
-p_80 <- c()
-p_both <- c()
+p_75_ks <- c()
+p_80_ks <- c()
+p_both_ks <- c()
+p_75_w <- c()
+p_80_w <- c()
+p_both_w <- c()
 
 for (i in 1:ncol(bs)){
-   p_75[i] <- ks.test(snp.sel_75, bs[,i])$p.value
-   p_80[i] <- ks.test(snp.sel_80, bs[,i])$p.value
-   p_both[i] <- ks.test(snp.sel_both, bs[,i])$p.value
+   p_75_ks[i] <- ks.test(snp.sel_75, bs[,i], alternative="greater")$p.value
+   p_80_ks[i] <- ks.test(snp.sel_80, bs[,i], alternative="greater")$p.value
+   p_both_ks[i] <- ks.test(snp.sel_both, bs[,i], alternative="greater")$p.value
+   p_75_w[i] <- wilcox.test(snp.sel_75, bs[,i], alternative="l", correct=TRUE)$p.value
+   p_80_w[i] <- wilcox.test(snp.sel_80, bs[,i], alternative="l", correct=TRUE)$p.value
+   p_both_w[i] <- wilcox.test(snp.sel_both, bs[,i], alternative="l", correct=TRUE)$p.value
 }
 
-length(which(p_75 < 0.05))
-length(which(p_80 < 0.05))
-length(which(p_both < 0.05))
+length(which(p_75_ks < 0.05))
+length(which(p_80_ks < 0.05))
+length(which(p_both_ks < 0.05))
+length(which(p_75_w < 0.05))
+length(which(p_80_w < 0.05))
+length(which(p_both_w < 0.05))
+# all are 1000
 
 out <- list()
 for (i in 1: ncol(bs)){
@@ -182,10 +208,10 @@ lines(densities.qtiles$x,densities.qtiles$q50, col="black", lwd=3 )
 lines(density(snp.sel_75, bw=0.05), col=alpha("firebrick3", 1), lwd=3)
 lines(density(snp.sel_80,bw=0.05), col=alpha("royalblue3", 1), lwd=3)
 lines(density(snp.sel_both,bw=0.05), col=alpha("darkorchid2", 1), lwd=3)
-abline(v=mean(avg_perm), lty=2, col= "black", lwd=3)
-abline(v=mean(snp.sel_75), lty=2, col= "firebrick3", lwd=3)
-abline(v=mean(snp.sel_80), lty=2, col= "royalblue3", lwd=3)
-abline(v=mean(snp.sel_both), lty=2, col= "darkorchid2", lwd=3)
+abline(v=mean(avg_perm), lty=2, col= "black", lwd=3) # mean: 0.459
+abline(v=mean(snp.sel_75)-0.01, lty=2, col= "firebrick3", lwd=3) # mean: 0.248  note, shifting this slightly so visible in plot.
+abline(v=mean(snp.sel_80), lty=2, col= "royalblue3", lwd=3) # mean: 0.251
+abline(v=mean(snp.sel_both), lty=2, col= "darkorchid2", lwd=3) # mean: 0.32
 
 legend("topright", c("neutral", "pH 7.5: selected", "pH 8.0: selected", "Overlapping selected"),
     col=c("black", "firebrick3", "royalblue3", "darkorchid2"), lty=1,
@@ -195,6 +221,5 @@ mtext(text=bquote(paste('(',italic('b'),')')),
         side=3, line=0,
              cex=1.5,
             at=par("usr")[1]-0.14*diff(par("usr")[1:2]), outer=FALSE)
-
 
 dev.off()
